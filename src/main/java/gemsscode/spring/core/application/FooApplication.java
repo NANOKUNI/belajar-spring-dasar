f59cf96ac81834e11e3b1373dc753f72a134f2fd
NANOKUNI <ccadangaan5@gmail.com>
2025-07-06 04:02:20 +0700

    Change Configuration

--- src/main/java/gemsscode/spring/core/application/FooApplication.java
+++ src/main/java/gemsscode/spring/core/application/FooApplication.java
@@ -1,14 +1,8 @@
 package gemsscode.spring.core.application;
 
 import gemsscode.spring.core.data.Foo;
-import gemsscode.spring.core.listener.AppStartingListener;
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class FooApplication {
@@ -21,16 +15,16 @@ public class FooApplication {
 //        System.out.println(foo);
 //    }
 
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(FooApplication.class);
-        application.setBannerMode(Banner.Mode.OFF);
-        application.setListeners(List.of(new AppStartingListener()));
-
-        ConfigurableApplicationContext applicationContext = application.run(args);
-
-        Foo foo = applicationContext.getBean(Foo.class);
-        System.out.println(foo);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication application = new SpringApplication(FooApplication.class);
+//        application.setBannerMode(Banner.Mode.OFF);
+//        application.setListeners(List.of(new AppStartingListener()));
+//
+//        ConfigurableApplicationContext applicationContext = application.run(args);
+//
+//        Foo foo = applicationContext.getBean(Foo.class);
+//        System.out.println(foo);
+//    }
 
     @Bean
     public Foo foo() {
